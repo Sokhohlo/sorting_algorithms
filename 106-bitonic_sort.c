cf6b1eb60f2ab861413e1f9813070588258e7349
sy1v1a <oyinyechi@ymail.com>
2023-07-25 04:07:15 -0700

    all

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -8,7 +8,8 @@
 void swap_ints(int *a, int *b);
 void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 char flow);
-void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_seq(int *array, size_t size,
+size_t start, size_t seq, char flow);
 void bitonic_sort(int *array, size_t size);
 
 /**
